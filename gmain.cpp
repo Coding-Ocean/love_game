a3ead543c421706530f60d2b0672ba2c8caeca97
Coding-Ocean <eijo524@gmail.com>
2026-07-12 09:42:35 +0900

    base

--- gmain.cpp
+++ gmain.cpp
@@ -2,82 +2,13 @@
 
 void gmain()
 {
-#if 1
 	window("love", 1920, 1080, full);
-#else
-	window("love", 1280, 720, win);
-#endif
 
-	float ox = width / 2.0f;
-	float oy = height / 2.0f;
-	float unit = width / 6.28f;
-
-	float ofstRad = 0.0f;
-	float radInc = 1.57f / 2.0f;
-
-	hideCursor();
 	while (!quit())
 	{
 		begin();
 
-		/*動かす---------------------------------*/
-		if (isPress(MOUSE_LBUTTON)) {
-			ox += mouseVx;
-			oy -= mouseVy;
-		}
-		unit += mouseWheel * 5;
-
-		/*描画する------------------------------*/
-		//背景
-		noStroke();
-		fill(20,20,20);
-		rectMode(CORNER);
-		rect(0, 0, width, height);
-		//XY軸
-		strokeWeight(1);
-		stroke(255,255,255);
-		mathAxis(ox, oy, unit);
-		//sin,cosカーブ
-		ofstRad += radInc * fixedDelta;
-		strokeWeight(10);
-		for(int deg = -180; deg<=180; deg+=3){
-			float rad = 3.1415926f / 180 * deg;
-			stroke(GREEN);
-			mathPoint(rad, cos(rad));
-			stroke(RED);
-			mathPoint(rad, sin(rad + ofstRad));
-		}
-		//sin,cosで円周に矩形
-		strokeWeight(3);
-		stroke(YELLOW);
-		rectMode(CENTER);
-		noFill();
-		for (int deg = 0; deg < 360; deg += 30) {
-			float rad = 3.1415926f / 180 * deg;
-			float px = cos(rad + ofstRad)*0.5f;
-			float py = sin(rad + ofstRad)*0.5f;
-			mathRect(px, py, 0.02f, 0.02f,rad+ofstRad);
-		}
-
-		//矢印
-		strokeWeight(3);
-		stroke(GRAY);
-		mathArrow(0, 0, mathMouseX, mathMouseY, 0.05f,20.f);
-		mathArc(1, 0, mathMouseX, mathMouseY, 0.2f);
-		//矢印の先にテキスト
-		fontRectMode(CENTER);
-		fontSize(30);
-		float2 v(mathMouseX, mathMouseY);
-		float2 ofst = v.normalize()*0.05f;
-		mathText("a", mathMouseX + ofst.x, mathMouseY + ofst.y);
-		
-		//テキスト
-		fontRectMode(CORNER);
-		fontSize(30);
 		printInfo();
-		print("mathMouseX:%.2f mathMouseY:%.2f", mathMouseX, mathMouseY);
-
 		end();
 	}
-	showCursor();
 }
